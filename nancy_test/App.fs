f70a729598fa3465ef4764fafbdf92b29f9ce2e4
janmaru <janmaru@gmail.com>
2016-05-24 23:24:53 +0200

    nancy test

---
+++ nancy_test/App.fs
@@ -0,0 +1,35 @@
+﻿//only for interactive
+//#r Will get things out of the GAC, but fully qualifying reference paths is a pain. 
+//You can use #I however to add a ‘search path’ to indicate where to look for references.
+#if INTERACTIVE
+#I @"../packages/"
+#r "Nancy.1.4.1/lib/net40/Nancy.dll"
+#r "Nancy.Hosting.Self.1.4.1/lib/net40/Nancy.Hosting.Self.dll"
+#endif
+
+namespace mahamudra.system.web
+
+open Nancy
+
+// :> obj Up-casting operator. Get function has to return an object
+type App() as this =
+    inherit NancyModule()
+    do
+        this.Get.["/"] <- fun _ -> "Hello World!" :> obj
+        this.Get.["/Fsharp"] <- fun _ -> "I can into F#" :> obj
+        this.Get.["/Json"] <- fun _ -> this.Response.AsJson([ "Test" ]) :> obj
+        this.Get.["/"] <- fun _ -> "Rewrite Hello World!" :> obj
+        this.Get.["/Complex"] <- fun _ -> 
+             let response = this.Response.AsJson(["This is my Response"])
+             response.ContentType <- "application/json"
+             response.Headers.Add("Funky-Header", "Funky-Header-Value")
+             response :> obj
+        this.Post.["/Post/{test}"] <- fun parameters -> 
+              let value = (parameters :?> Nancy.DynamicDictionary).["test"]
+              let response = this.Response.AsJson([ sprintf "test %O" value ])
+              response :> obj
+        this.Get.["/foo", true] <- fun ctx ct ->
+          async {
+              return "bar" :> obj
+          }
+          |> Async.StartAsTask
---
+++ nancy_test/Program.fs
@@ -0,0 +1,34 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+open Nancy.Hosting.Self
+open Nancy
+open mahamudra.system.web
+
+[<EntryPoint>]
+let main args = 
+////Namespace Reservations
+////Windows requires applications to register the part of the HTTP URL namespace, that they will be, listening on, or for the application to run with administrator privileges. This is how the MSDN documentation describes the requirement
+////Namespace reservation assigns the rights for a portion of the HTTP URL namespace to a particular group of users. A reservation gives those users the right to create services that listen on that portion of the namespace. Reservations are URL prefixes, meaning that the reservation covers all sub-paths of the reservation path.
+////Failing to do so is going to result in an access denied, from the operating system, when the application tries to open and listen on a certain url and port.
+//    let nancy = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:" + "8100"))
+    let hostConfigs = new HostConfiguration()
+    hostConfigs.UrlReservations.CreateAutomatically <- true 
+    StaticConfiguration.DisableErrorTraces <- false
+////    'use' bindings are not permitted in modules and are treated as 'let' bindings
+//    use nancy = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:" + "8101/nancy/"),
+//                                                 new DefaultNancyBootstrapper(), 
+//                                                 hostConfigs)
+//    let nancy = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:" + "8101/"),
+//                                                 new DefaultNancyBootstrapper(), 
+//                                                 hostConfigs)
+    let uri =new Uri("http://localhost:" + "8101/");
+    let nancy = new Nancy.Hosting.Self.NancyHost(uri)
+    //netsh http add urlacl url="http://+:8101/" user="Everyone"
+    //netsh http delete urlacl url=http://+:8101/
+    nancy.Start()
+    Console.WriteLine("Server started at:" + System.DateTime.Now.ToString() + " on url: " + uri.ToString())
+    while true do Console.ReadLine() |> ignore
+    nancy.Stop()
+    0
